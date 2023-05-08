ac05e4f8ebbd7008dcf247b80a2a2aa4f10a6737
OksanaLesjuk <126483028+OksanaLesjuk@users.noreply.github.com>
2023-05-08 23:02:57 +0300

    стилі header, hero, offeritg

--- css/README.md
+++
@@ -1,1 +0,0 @@
-# foodsta-team-project
--- js/slide.js
+++ js/slide.js
@@ -1,10 +1,7 @@
 
 $('.autoplay').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-});
-
-if (totalSpent >= 50000) { discount = SILVER_DISCOUNT }
-if else (totalSpent >= 20000 && totalSpent < 50000){ discount = SILVER_DISCOUNT } if else (totalSpent >= 5000 && totalSpent < 20000){ discount = BRONZE_DISCOUNT } else { BASE_DISCOUNT }
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
